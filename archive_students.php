4615cd77ec1588f1fbdd7175e94c1a5001a2a88f
Julia Polushina <287jpi@gmail.com>
2018-05-12 14:45:07 +0300

    form with students

--- archive_students.php
+++ archive_students.php
@@ -5,36 +5,28 @@ function archive_all_students()
     return drupal_get_form('archive_all_students_page');
 }
 
-function archive_all_students_page()
+function archive_all_students_page($form, &$form_state)
 {
-    $form = array();
     $years = get_years();
     $array = array();
     $i = 0;
     foreach ($years as $value) {
         $array[$i++] = $value->year;
     }
-    $form['archive_all_students_page']['year'] = array(
+    $form['year'] = array(
         '#type' => 'select',
         '#title' => t('Год'),
         '#options' => $array,
+        '#default_value' => 0,
         '#ajax' => array(
             'event' => 'change',
-            'callback' => 'archive_all_students_tables_page',
+            'callback' => 'archive_all_students_dropdown_callback',
             'wrapper' => 'student-wrapper',
+            'method' => 'replace',
+            'effect' => 'fade',
         ),
     );
 
-    $form['archive_all_students_page']['wrapper'] = array(
-        '#prefix' => '<div id="student-wrapper">',
-        '#suffix' => '</div>',
-    );
-    return $form;
-}
-
-function archive_all_students_tables_page($form, $form_state)
-{
-    $year = $form_state['values']['year'][0]['value'];
     $header = array(
         array('data' => t('ФИО'), 'field' => 's.last_name'),
         array('data' => t('Группа'), 'field' => 'g.group_number'),
@@ -44,11 +36,32 @@ function archive_all_students_tables_page($form, $form_state)
         array('data' => t('Итоговая оценка'), 'field' => 'dip.final_evaluation'),
     );
 
-    $nodes = get_student_by_year($header, $year);
+    $nodes = get_all_students($header);
+
+    $form['simple_table'] = fill_table($form, $nodes, $header);
+    // Подключаем отображение пейджинатора.
+    $form['pager']['#markup'] = theme('pager');
+    return $form;
+}
+
+function archive_all_students_dropdown_callback($form, $form_state)
+{
+    $year = $form_state['complete form']['year']['#options'][$form_state['values']['year']];
+
+    $nodes = get_student_by_year($form['simple_table']['#header'], $year);
+
+    $form['simple_table'] = fill_table($form, $nodes, $form['simple_table']['#header']);
+    return $form['simple_table'];
+}
+
+function fill_table($form, $nodes, $header)
+{
     $form['simple_table'] = array(
         '#type' => 'container',
         '#theme' => 'simple',
         '#header' => $header,
+        '#prefix' => '<div id="student-wrapper">',
+        '#suffix' => '</div>',
     );
 
     foreach ($nodes as $nid => $node) {
@@ -73,10 +86,7 @@ function archive_all_students_tables_page($form, $form_state)
             '#markup' => $node->final_evaluation,
         );
     }
-    // Подключаем отображение пейджинатора.
-    $form['pager']['#markup'] = theme('pager');
-
-    return $form;
+    return $form['simple_table'];
 }
 
 function get_years()
@@ -110,7 +120,29 @@ function get_student_by_year($header, $year)
         ->fields('d', array('direction_code', 'direction_name'))
         ->fields('dip', array('final_evaluation', 'date_protect'))
         ->condition('s.`year`', $year)
-        ->limit(10)
+        ->limit(5)
+        ->orderByHeader($header);
+    $students = $query1->execute()
+        ->fetchAll();
+
+    db_set_active();
+    return $students;
+}
+
+function get_all_students($header)
+{
+    db_set_active('archive_db');
+    $query1 = db_select('student', 's')
+        ->extend('PagerDefault')
+        ->extend('TableSort');
+    $query1->leftJoin('stud_group', 'g', 'g.id_group = s.id_group AND g.`year` = s.`year`');
+    $query1->leftJoin('direction', 'd', 'g.id_direction = d.id_direction AND d.`year` = g.`year`');
+    $query1->innerJoin('teacher_student_diplom', 'dip', 's.id_student = dip.id_student AND s.`year` =dip.`year`');
+    $query1->fields('s')
+        ->fields('g', array('group_number'))
+        ->fields('d', array('direction_code', 'direction_name'))
+        ->fields('dip', array('final_evaluation', 'date_protect'))
+        ->limit(5)
         ->orderByHeader($header);
     $students = $query1->execute()
         ->fetchAll();
